09382905dafc1b0e38fce11f873da77494270489
Daniel Mariano <60660967+Marianodan@users.noreply.github.com>
2022-04-01 19:14:13 -0300

    Var, Let e Const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3 // Trabalhar com var não é o ideal, pois vc poderá mudar a variavel a qualquer momento do código
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c = 50
+console.log(c)
